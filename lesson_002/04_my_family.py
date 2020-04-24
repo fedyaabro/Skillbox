2685b224e0ade8e9b4be3b8dca6bbdf915d10f39
fedyaabro <fedor.abrosimov94@gmail.com>
2020-04-24 17:19:02 +0300

    Выполнил задание 04

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -2,19 +2,23 @@
 # -*- coding: utf-8 -*-
 
 # Создайте списки:
-
+from pprint import pprint
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
-my_family = []
+my_family = ['mother', 'father', 'me']
 
 
 # список списков приблизителного роста членов вашей семьи
 my_family_height = [
-    # ['имя', рост],
-    [],
+    [my_family[0], 153],
+    [my_family[1], 178],
+    [my_family[2], 191],
 ]
 
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
-
+print('father height = ', my_family_height[1][1])
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+
+total_height = my_family_height[0][1] + my_family_height[1][1] + my_family_height[2][1]
+print(total_height)
