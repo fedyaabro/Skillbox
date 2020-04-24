67b8db5305d078a70389993e5f81affc0d773dce
fedyaabro <fedor.abrosimov94@gmail.com>
2020-04-24 17:04:10 +0300

    Выполнил задание 03

--- lesson_002/03_favorite_movies.py
+++ lesson_002/03_favorite_movies.py
@@ -14,4 +14,13 @@ my_favorite_movies = 'Терминатор, Пятый элемент, Ават�
 # Переопределять my_favorite_movies и использовать .split() нельзя.
 # Запятая не должна выводиться.
 
-# TODO здесь ваш код
+first_movie = my_favorite_movies[0:10]
+last_movie = my_favorite_movies[-15:]
+second_movie = my_favorite_movies[12:25]
+second_from_end = my_favorite_movies[-22:-17]
+
+
+print(first_movie)
+print(last_movie)
+print(second_movie)
+print(second_from_end)
