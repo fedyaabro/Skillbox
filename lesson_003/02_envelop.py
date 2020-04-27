7d0d2af476c54eb705ec9dddd0c04574ee2cef1b
fedyaabro <fedor.abrosimov94@gmail.com>
2020-04-27 19:23:54 +0300

    Выполнил задание 02 envelop and brick

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -10,7 +10,7 @@
 # Использовать только операторы if/elif/else, можно вложенные
 
 envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+# paper_x, paper_y = 8, 9
 # проверить для
 # paper_x, paper_y = 9, 8
 # paper_x, paper_y = 6, 8
@@ -20,15 +20,19 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+# if envelop_x >= paper_x and envelop_y >= paper_y:
+#     print('ДА')
+# elif envelop_x >= paper_y and envelop_y >= paper_x:
+#     print('ДА')
+# else:
+#     print('НЕТ')
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
 # могут быть в диапазоне от 1 до 1000)
-#
-# Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
+# Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
+hole_x, hole_y = 8, 9
 # brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
@@ -44,9 +48,21 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 11, 3, 6
 # brick_x, brick_y, brick_z = 11, 6, 3
 # brick_x, brick_y, brick_z = 6, 11, 3
-# brick_x, brick_y, brick_z = 6, 3, 11
+brick_x, brick_y, brick_z = 6, 3, 11
 # brick_x, brick_y, brick_z = 3, 6, 11
 # brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if hole_x >= brick_x and hole_y >= brick_z:
+    print('ВЛЕЗЕТ')
+elif hole_x >= brick_z and hole_y >= brick_x:
+    print('ВЛЕЗЕТ')
+elif hole_x >= brick_x and hole_y >= brick_y:
+    print('ВЛЕЗЕТ')
+elif hole_x >= brick_z and hole_y >= brick_y:
+    print('ВЛЕЗЕТ')
+elif hole_x >= brick_y and hole_y >= brick_z:
+    print('ВЛЕЗЕТ')
+else:
+    print('НЕ ВЛЕЗЕТ')
+
