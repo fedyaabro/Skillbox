83f0ae97104168622c1a6e84418b8cef523f183e
fedyaabro <fedor.abrosimov94@gmail.com>
2020-04-29 22:46:50 +0300

    Выполнил задание 06 rainbow

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -20,20 +20,20 @@ def bubble_draw(point, step):
     radius = 50
     for _ in range(3):
         radius += step
-        sd.circle(center_position=point, radius=radius, width=2,color=sd.random_color())
+        sd.circle(center_position=point, radius=radius, width=2, color=sd.random_color())
 
 
 point = sd.get_point(300, 300)
 
 # Нарисовать 10 пузырьков в ряд
-# for x in range(100, 1001, 100):
-#     point = sd.get_point(x, 100)
-#     bubble_draw(point=point, step=5)
-# # Нарисовать три ряда по 10 пузырьков
-# for y in range(100, 301, 100):
-#     for x in range(100, 1001, 100):
-#         point = sd.get_point(x, y)
-#         bubble_draw(point=point, step=5)
+for x in range(100, 1001, 100):
+    point = sd.get_point(x, 100)
+    bubble_draw(point=point, step=5)
+# Нарисовать три ряда по 10 пузырьков
+for y in range(100, 301, 100):
+    for x in range(100, 1001, 100):
+        point = sd.get_point(x, y)
+        bubble_draw(point=point, step=5)
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 for _ in range(100):
     point = sd.random_point()
--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -8,11 +8,26 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+# x_start, x_end = 50, 450
+# step = 5
+# i = 0
+# for _ in rainbow_colors:
+#     start = sd.get_point(x_start, 50)
+#     end = sd.get_point(x_end, 550)
+#     sd.line(start_point=start, end_point=end, color=rainbow_colors[i], width=5)
+#     i += 1
+#     x_start += step
+#     x_end += step
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
 # TODO здесь ваш код
-
+point = sd.get_point(500, 0)
+radius = 300
+i = 0
+for _ in rainbow_colors:
+    radius += 30
+    sd.circle(center_position=point, radius=radius, color=rainbow_colors[i], width=30)
+    i += 1
 sd.pause()
