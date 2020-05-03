53ec8771cce0cb216f0fb3bd18f64c0190db496f
MikhailGusarov <vgg639@gmail.com>
2020-05-03 16:15:43 +0300

    Модуль 3. 07_wall замечания 1

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -3,9 +3,7 @@
 # (цикл for)
 import simple_draw as sd
 
-sd.resolution = (600, 600)
-
-
+sd.resolution = (600, 600)  # TODO зачем эта строка
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
@@ -19,12 +17,9 @@ brick_height = 50
 
 for brick_x in range(0, wall_width, brick_width):
     left_bottom = sd.get_point(brick_x, 0)
-    right_top = sd.get_point(brick_width, brick_height)
+    right_top = sd.get_point(brick_width, brick_height)  # TODO почему тут brick_width
     sd.rectangle(left_bottom=left_bottom, right_top=right_top, width=1)
-# жду твои тудушки, можешь считать себя прорабом
 
-
-
-
-
+# TODO перед тем как показывать код, нужно убедится, что ты вообще понимаешь каждую строчку, что она выполняет и тд.
+# TODO пока у тебя ошибки аля поделки подделки
 sd.pause()
