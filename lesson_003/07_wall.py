56afc147493110e42c363b976f0437c1a7299d86
MikhailGusarov <vgg639@gmail.com>
2020-05-03 16:44:20 +0300

    Модуль 3. 07_wall сломал программу уменьшив ширину стены на 50 пикселей

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -3,23 +3,26 @@
 # (цикл for)
 import simple_draw as sd
 
-# TODO sd.resolution убрал, требований по размеру экрана нет + в этой роли переменные wall_width и wall_height
+# TODO увеличил размер окна, а то он меньше размера стены
+sd.resolution = (1000, 800)
+
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
-
-wall_width = 700
+wall_width = 650  # TODO изменил ширину стены. тем самым сломав тебе программу. Исправляй!!!
 wall_height = 700
 brick_width = 100
 brick_height = 50
 
+# TODO сделал границы стены
+left_bottom_wall = sd.get_point(0, 0)
+right_top_wall = sd.get_point(wall_width, wall_height)
+sd.rectangle(left_bottom=left_bottom_wall, right_top=right_top_wall, width=1)
+
 for brick_x in range(0, wall_width, brick_width):
     left_bottom = sd.get_point(brick_x, 0)
     right_top = sd.get_point(brick_width + brick_x, brick_height)
-    # TODO координаты верхней правой точки = x + ширина кирвича, y + высота кирпича, поторопился и наебался
     sd.rectangle(left_bottom=left_bottom, right_top=right_top, width=1)
 
-# TODO пока у тебя ошибки аля поделки подделки - четкая кепка. Теперь строится нижний ряд нормально
 sd.pause()
