39f92cfd79a81ef649aa6259c47f810586830db0
fedyaabro <fedor.abrosimov94@gmail.com>
2020-05-04 19:33:11 +0300

    Выполнил 07 - wall

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -6,23 +6,29 @@ import simple_draw as sd
 # TODO увеличил размер окна, а то он меньше размера стены
 sd.resolution = (1000, 800)
 
-
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-wall_width = 650  # TODO изменил ширину стены. тем самым сломав тебе программу. Исправляй!!!
-wall_height = 700
+wall_width = 750
+wall_height = 750
 brick_width = 100
 brick_height = 50
 
-# TODO сделал границы стены
-left_bottom_wall = sd.get_point(0, 0)
+# TODO сделать так, чтобы куски кирпичей не вылезали за пределы стены
+
+left_bottom_wall = sd.get_point(100, 100)
 right_top_wall = sd.get_point(wall_width, wall_height)
 sd.rectangle(left_bottom=left_bottom_wall, right_top=right_top_wall, width=1)
-
-for brick_x in range(0, wall_width, brick_width):
-    left_bottom = sd.get_point(brick_x, 0)
-    right_top = sd.get_point(brick_width + brick_x, brick_height)
-    sd.rectangle(left_bottom=left_bottom, right_top=right_top, width=1)
-
+shift_index = 0
+for brick_y in range(100, wall_height, brick_height):
+    for brick_x in range(100, wall_width, brick_width):
+        if shift_index % 2 == 0:
+            left_bottom = sd.get_point(brick_x, brick_y)
+            right_top = sd.get_point(brick_x + brick_width, brick_height + brick_y)
+            sd.rectangle(left_bottom=left_bottom, right_top=right_top, width=2)
+        else:
+            left_bottom = sd.get_point(brick_x - (brick_width * 0.5), brick_y)
+            right_top = sd.get_point(brick_x + (brick_width * 0.5), brick_height + brick_y)
+            sd.rectangle(left_bottom=left_bottom, right_top=right_top, width=2)
+    shift_index += 1
 sd.pause()
