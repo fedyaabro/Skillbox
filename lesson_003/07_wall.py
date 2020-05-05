2c2f8d515db4e2c4452bda9fd35fb1b68d69756d
fedyaabro <fedor.abrosimov94@gmail.com>
2020-05-05 16:22:52 +0300

    Изменил размер стены

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -9,8 +9,8 @@ sd.resolution = (1000, 800)
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-wall_width = 750
-wall_height = 750
+wall_width = 700
+wall_height = 700
 brick_width = 100
 brick_height = 50
 
@@ -19,10 +19,11 @@ brick_height = 50
 left_bottom_wall = sd.get_point(100, 100)
 right_top_wall = sd.get_point(wall_width, wall_height)
 sd.rectangle(left_bottom=left_bottom_wall, right_top=right_top_wall, width=1)
-shift_index = 0
+# different_block = wall_width % brick_width
+brick_shift_index = 0
 for brick_y in range(100, wall_height, brick_height):
     for brick_x in range(100, wall_width, brick_width):
-        if shift_index % 2 == 0:
+        if brick_shift_index % 2 == 0:
             left_bottom = sd.get_point(brick_x, brick_y)
             right_top = sd.get_point(brick_x + brick_width, brick_height + brick_y)
             sd.rectangle(left_bottom=left_bottom, right_top=right_top, width=2)
@@ -30,5 +31,5 @@ for brick_y in range(100, wall_height, brick_height):
             left_bottom = sd.get_point(brick_x - (brick_width * 0.5), brick_y)
             right_top = sd.get_point(brick_x + (brick_width * 0.5), brick_height + brick_y)
             sd.rectangle(left_bottom=left_bottom, right_top=right_top, width=2)
-    shift_index += 1
+    brick_shift_index += 1
 sd.pause()
