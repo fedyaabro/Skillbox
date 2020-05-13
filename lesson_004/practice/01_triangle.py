93cb2a2467cfe348bab3f8a7227947e6d061f8d4
fedyaabro <fedor.abrosimov94@gmail.com>
2020-05-13 23:37:34 +0300

    сделал функцию для треугольника

--- lesson_004/practice/01_triangle.py
+++ lesson_004/practice/01_triangle.py
@@ -4,35 +4,27 @@
 
 import simple_draw as sd
 
-# нарисовать треугольник из точки (300, 300) с длиной стороны 200
-length = 200
-point = sd.get_point(300, 300)
 
-# v1 = sd.get_vector(start_point=point, angle=0, length=200, width=3)
-# v1.draw()
-#
-# v2 = sd.get_vector(start_point=v1.end_point, angle=120, length=200, width=3)
-# v2.draw()
-#
-# v3 = sd.get_vector(start_point=v2.end_point, angle=240, length=200, width=3)
-# v3.draw()
+# нарисовать треугольник из точки (300, 300) с длиной стороны 200
+# length = 200
+# angle = 0
+# point = sd.get_point(300, 300)
+# side_1 = sd.get_vector(start_point=point, angle=angle, length=200)
+# side_1.draw()
+# side_2 = sd.get_vector(start_point=side_1.end_point, angle=angle + 120, length=200)
+# side_2.draw()
+# side_3 = sd.get_vector(start_point=side_2.end_point, angle=angle + 240, length=200)
+# side_3.draw()
+
 
 # определить функцию рисования треугольника из заданной точки с заданным наклоном
-def triangle(point, angle=0):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=200, width=3)
-    v1.draw()
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=200, width=3)
-    v2.draw()
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=200, width=3)
-    v3.draw()
+def triangle(start_point=sd.get_point(200, 200), angle=0):
+    for angle in range(0, 360, 120):
+        side = sd.get_vector(start_point=start_point, angle=angle)
+        side.draw()
+        start_point = side.end_point
 
 
-point_0 = sd.get_point(300, 300)
-
-for angle in range(0, 361, 30):
-    triangle(point=point_0, angle=angle)
+triangle()
 
 sd.pause()
-
