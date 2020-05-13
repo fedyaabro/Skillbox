f93bc508358efe10f25c27ae49ce42864c9c503e
fedyaabro <fedor.abrosimov94@gmail.com>
2020-05-13 23:49:53 +0300

    из-за цикла в функции не было возможности изменять угол(как я подумал)

--- lesson_004/practice/01_triangle.py
+++ lesson_004/practice/01_triangle.py
@@ -19,12 +19,19 @@ import simple_draw as sd
 
 # определить функцию рисования треугольника из заданной точки с заданным наклоном
 def triangle(start_point=sd.get_point(200, 200), angle=0):
-    for angle in range(0, 360, 120):
-        side = sd.get_vector(start_point=start_point, angle=angle)
-        side.draw()
-        start_point = side.end_point
+    """Draw triangle from start point and angle"""
+    # for angle in range(0, 361, 120):
+    #     side = sd.get_vector(start_point=start_point, length=200, angle=angle)
+    #     side.draw()
+    #     start_point = side.end_point
+    side_1 = sd.get_vector(start_point=start_point, angle=angle, length=200)
+    side_1.draw()
+    side_2 = sd.get_vector(start_point=side_1.end_point, angle=angle + 120, length=200)
+    side_2.draw()
+    side_3 = sd.get_vector(start_point=side_2.end_point, angle=angle + 240, length=200)
+    side_3.draw()
 
 
-triangle()
-
+for angle in range(0, 361, 30):
+    triangle(angle=angle)
 sd.pause()
